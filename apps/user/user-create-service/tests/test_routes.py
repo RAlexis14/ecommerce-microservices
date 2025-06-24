cd5ecb4022e2cbed10c137b1d22dd2c88ff6a477
RAlexis02 <alexis.amazonas@gmail.com>
2025-06-24 16:08:35 -0500

    test(user-create): fix import path for pytest compatibility

--- apps/user/user-create-service/tests/test_routes.py
+++ apps/user/user-create-service/tests/test_routes.py
@@ -1,6 +1,11 @@
 # tests/test_routes.py
 
+import sys
+import os
 import pytest
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import create_app
 
 app = create_app()
