88c28184a1f586f3e6a357baae81e0ceda4d1142
RAlexis02 <alexis.amazonas@gmail.com>
2025-07-14 10:27:05 -0500

    fix(order): rename getOrders to getOrdersByUserId and update route import

--- apps/order/order-placement-service/src/routes/order.routes.js
+++ apps/order/order-placement-service/src/routes/order.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createOrder, getOrders } from '../controllers/order.controller.js';
+import { createOrder, getOrdersByUserId } from '../controllers/order.controller.js';
 
 const router = express.Router();
 
